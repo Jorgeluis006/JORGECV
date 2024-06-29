63dc1c5b612541ed480dcb2054cb1f10d7db7442
Jorgeluis006 <cuentaparajugarenlan007@gmail.com>
2024-06-29 16:26:16 -0500

    colornegro

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography';
 // Define el componente de encabezado
 const Header = () => {
   return (
-    <Typography variant="h3" sx={{ color: "white", textAlign: "center", paddingTop: "30px", position: "fixed", width: "100%", top: 0, zIndex: 1000, backgroundColor: "red", left: 0, right: 0, margin: "0 auto", borderRadius: "9px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+    <Typography variant="h3" sx={{ color: "white", textAlign: "center", paddingTop: "30px", position: "fixed", width: "100%", top: 0, zIndex: 1000, backgroundColor: "black", left: 0, right: 0, margin: "0 auto", borderRadius: "9px", display: "flex", alignItems: "center", justifyContent: "center" }}>
       <span style={{ flexGrow: 1 }}>🄼🅈 🄻🄸🄵🄴 🄹🄾🅁🄶🄴 🄴🅂🄲🄾🄱🄰🅁</span>
       <button style={{ left:1650, color: "black", backgroundColor: "white", borderRadius: "5px", padding: "1px 30px", cursor: "pointer",top:15,position:"absolute" }}>Button</button>
     </Typography>
